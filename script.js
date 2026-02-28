97a08eec0515da0961367a5ca4ddfea24dbdce4f
Dking <dhcdouglas.oc@gmail.com>
2026-02-28 20:31:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollContato() {
+  document.getElementById("contato").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+const form = document.getElementById("formContato");
+const msg = document.getElementById("msg");
+
